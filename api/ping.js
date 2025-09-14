91601aa0d05c9bd40aa3cc8e241238b96ce54477
ianvs31 <ryangao31@gmail.com>
2025-09-14 22:12:31 +0800

    chore(api): add /api/ping for routing diagnostics

--- api/ping.js
+++ api/ping.js
@@ -1,6 +1,6 @@
 module.exports = async function handler(req, res) {
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  return res.status(200).end(JSON.stringify({ ok: true, path: req.url, now: Date.now() }));
+  return res.status(200).send(JSON.stringify({ ok: true, method: req.method, time: Date.now() }));
 };
 
 
