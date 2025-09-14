7641b512334bce1eb328ecdca570f2d89b45dda0
ianvs31 <ryangao31@gmail.com>
2025-09-14 22:55:55 +0800

    renew

--- api/ping.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  return res.status(200).send(JSON.stringify({ ok: true, method: req.method, time: Date.now() }));
-};
-
-
